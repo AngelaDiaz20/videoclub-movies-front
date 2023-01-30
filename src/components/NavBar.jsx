97224c70567de4cec7459a55d6d4f60e242b09af
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-01-30 16:33:37 -0500

    header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { AppBar, Toolbar, styled } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const Header = styled(AppBar)
+    `background: #001320`;
+
+const Tabs = styled(NavLink)
+    `font-size:20px;
+    margin-right: 40px;
+    color: inherit;
+    text-decoration: none;`
+
 
 const Navbar = () => {
 
     return (
-        <div>Navbar</div>
+        <>
+            <Header position='static'>
+                <Toolbar>
+                    {/* <Tabs to='/'>Lista de películas</Tabs>
+                    <Tabs to='/add'>Agregar pelicula</Tabs> */}
+                </Toolbar>
+            </Header>
+            <div>Navbar</div>
+        </>
     );
 
 };
