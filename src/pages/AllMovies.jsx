a4dbef7d4f92ac9d81e6c438d6954d82e2679db2
Jlbejarano662 <jasmin.1207@hotmail.com>
2023-01-31 14:40:26 -0500

    improved allmovies and footer styles

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -1,27 +1,26 @@
 import { useEffect, useState, useMemo } from "react";
 import { Link } from "react-router-dom";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 import "./allMovies.css";
 
 import { getMovies, deleteMovie } from "../service/api";
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment'
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
 
 import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
-import { AiOutlineSearch } from "react-icons/ai"
+import { AiOutlineSearch } from "react-icons/ai";
 
 const AllMovies = () => {
-
   // const [movies, setMovies] = useState(data);
   const [movies, setMovies] = useState([]);
   const [inputTitle, setInputTitle] = useState("");
-  
+
   const filteredMovies = useMemo(() => {
-        return movies.filter(item => (
-            item.title.toLowerCase().includes(inputTitle.toLowerCase())
-        ))
-  },[inputTitle, movies]);
+    return movies.filter((item) =>
+      item.title.toLowerCase().includes(inputTitle.toLowerCase())
+    );
+  }, [inputTitle, movies]);
 
   useEffect(() => {
     getAllMovies();
@@ -34,35 +33,36 @@ const AllMovies = () => {
 
   const deleteMovieDetails = async (id) => {
     Swal.fire({
-      title: '¿Estás seguro de querer eliminar esta película?',
+      title: "¿Estás seguro de querer eliminar esta película?",
       text: "¡No podrás revertir esta acción!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      cancelButtonText: 'Cancelar',
-      confirmButtonText: 'Si, ¡bórrala!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Si, ¡bórrala!",
     }).then((result) => {
       if (result.isConfirmed) {
         deleteMovie(id);
         Swal.fire(
-          '¡Película eliminada!',
+          "¡Película eliminada!",
           `El registro de esta película ha sido borrado con éxito`,
-          'success'
-        )
+          "success"
+        );
         getAllMovies();
       }
-    })
-  }; 
+    });
+  };
 
   return (
-    <div className="table-responsive">
-        <TextField
+    <div className="all-movies">
+      <div className="search">
+      <TextField
         id="searchInput"
         value={inputTitle}
-        placeholder="Buscar por titulo"
+        placeholder=" Buscar por titulo"
         onChange={(e) => setInputTitle(e.target.value)}
-        label="Outlined"
+        label=""
         InputProps={{
           startAdornment: (
             <InputAdornment position="end">
@@ -71,47 +71,50 @@ const AllMovies = () => {
           ),
         }}
         variant="outlined"
-        />
-      <table className="table">
-        <thead>
-          <tr className="table-head">
-            <th>ID</th>
-            <th>TÍTULO</th>
-            <th>AÑO</th>
-            <th>TIEMPO</th>
-            <th>LENGUAJE</th>
-            <th>FECHA</th>
-            <th>PAÍS</th>
-            <th>ACCIONES</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredMovies.map((movie) => (
-            <tr key={movie._id} className="table-body">
-              <td>{movie.id}</td>
-              <td>{movie.title}</td>
-              <td>{movie.year}</td>
-              <td>{movie.time}</td>
-              <td>{movie.language}</td>
-              <td>{movie.release}</td>
-              <td>{movie.country}</td>
-              <td>
-                <div className="actions">
-                  <Link to={`/edit/${movie._id}`} className="btn edit">
-                    <BsFillPencilFill className="icon" />
-                  </Link>
-                  <Link
-                    className="btn delete"
-                    onClick={() => deleteMovieDetails(movie._id)}
-                  >
-                  <BsFillTrashFill className="icon" />
-                  </Link>
-                </div>
-              </td>
+      />
+      </div>
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr className="table-head">
+              <th>ID</th>
+              <th>TÍTULO</th>
+              <th>AÑO</th>
+              <th>TIEMPO</th>
+              <th>LENGUAJE</th>
+              <th>FECHA</th>
+              <th>PAÍS</th>
+              <th>ACCIONES</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredMovies.map((movie) => (
+              <tr key={movie._id} className="table-body">
+                <td>{movie.id}</td>
+                <td>{movie.title}</td>
+                <td>{movie.year}</td>
+                <td>{movie.time}</td>
+                <td>{movie.language}</td>
+                <td>{movie.release}</td>
+                <td>{movie.country}</td>
+                <td>
+                  <div className="actions">
+                    <Link to={`/edit/${movie._id}`} className="button edit">
+                      <BsFillPencilFill className="icon" />
+                    </Link>
+                    <Link
+                      className="button delete"
+                      onClick={() => deleteMovieDetails(movie._id)}
+                    >
+                      <BsFillTrashFill className="icon" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
