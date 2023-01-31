336caba281194583e8f4ebe59548b783dd0fa98d
Jlbejarano662 <jasmin.1207@hotmail.com>
2023-01-30 19:00:12 -0500

    styles

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -1,34 +1,13 @@
 import { useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  styled,
-  Button,
-} from "@mui/material";
 
+/* import { getMovies, deleteMovie } from "../service/api"; */
 
 import { Link } from "react-router-dom";
 
-const StyledTable = styled(Table)`
-  width: 90%;
-  margin: 50px auto 0 auto;
-`;
-const THead = styled(TableRow)`
-  background: #000000;
-  & > th {
-    color: #ffffff;
-    font-size: 20px;
-  }
-`;
+import "./allMovies.css";
 
-const TBody = styled(TableRow)`
-  & > td {
-    font-size: 20px;
-  }
-`;
+import { BsFillPencilFill, BsFillTrashFill } from "react-icons/bs";
+
 const AllMovies = () => {
   const data = [
     {
@@ -58,9 +37,90 @@ const AllMovies = () => {
       release: "01-01-2023",
       country: "uk",
     },
+    ,
+    {
+      id: 4,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 5,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 6,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 7,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 8,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 9,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 10,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
+    ,
+    {
+      id: 11,
+      title: "Título de la película",
+      year: 1999,
+      time: 22,
+      language: "Español",
+      release: "01-01-2023",
+      country: "uk",
+    },
   ];
-  const [movies, setMovies] = useState(data);
 
+  const [movies, setMovies] = useState(data);
+  /* const [movies, setMovies] = useState([]); */
 
   useEffect(() => {
     getAllMovies();
@@ -70,51 +130,58 @@ const AllMovies = () => {
     setMovies(data);
   };
 
+  /*  
+  const getAllMovies = async () => {
+    let response = await getMovies();
+    setMovies(response.data);
+  };
+  const deleteMovieDetails = async (id) => {
+    await deleteMovie(id);
+    getAllMovies();
+  }; */
   return (
-    <StyledTable>
-      <TableHead>
-        <THead>
-          <TableCell>Id</TableCell>
-          <TableCell>Titulo</TableCell>
-          <TableCell>Año</TableCell>
-          <TableCell>Tiempo</TableCell>
-          <TableCell>Lenguaje</TableCell>
-          <TableCell>Fecha de lanzamiento</TableCell>
-          <TableCell>Pais</TableCell>
-          <TableCell></TableCell>
-        </THead>
-      </TableHead>
-      <TableBody>
-        {movies.map((movie) => (
-          <TBody key={movie._id}>
-            <TableCell>{movie.id}</TableCell>
-            <TableCell>{movie.title}</TableCell>
-            <TableCell>{movie.year}</TableCell>
-            <TableCell>{movie.time}</TableCell>
-            <TableCell>{movie.language}</TableCell>
-            <TableCell>{movie.release}</TableCell>
-            <TableCell>{movie.country}</TableCell>
-            <TableCell>
-              <Button
-                variant="contained"
-                style={{ marginRight: 10 }}
-                component={Link}
-                to={`/edit/${movie._id}`}
-              >
-                Edit
-              </Button>
-              <Button
-                variant="contained"
-                color="secondary"
-                /* onClick={() => deleteMovieDetails(movie._id)} */
-              >
-                Delete
-              </Button>
-            </TableCell>
-          </TBody>
-        ))}
-      </TableBody>
-    </StyledTable>
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr className="table-head">
+            <th>ID</th>
+            <th>TÍTULO</th>
+            <th>AÑO</th>
+            <th>TIEMPO</th>
+            <th>LENGUAJE</th>
+            <th>FECHA</th>
+            <th>PAÍS</th>
+            <th>ACCIONES</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie._id} className="table-body">
+              <td>{movie.id}</td>
+              <td>{movie.title}</td>
+              <td>{movie.year}</td>
+              <td>{movie.time}</td>
+              <td>{movie.language}</td>
+              <td>{movie.release}</td>
+              <td>{movie.country}</td>
+              <td>
+                <div className="actions">
+                  <Link to={`/edit/${movie._id}`} className="btn edit">
+                    <BsFillPencilFill className="icon" />
+                  </Link>
+                  <Link
+                    className="btn delete"
+                    /* onClick={() => deleteMovieDetails(movie._id)} */
+                  >
+                    <BsFillTrashFill className="icon" />
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
