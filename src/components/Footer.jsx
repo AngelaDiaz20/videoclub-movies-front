47b3db63c41fb0fc94a3acdff1f8ad51ac9baac8
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-01-31 12:13:20 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import './Footer.css'
 
 const Footer = () => {
 
     return (
-        <div>Footer</div>
+        <>
+            <footer>
+                <div className="text">©VIDEOCLUB</div> <hr/>
+                <div className="icons">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-square-twitter"></i>
+                    <i class="fa-brands fa-square-youtube"></i>
+                    <i class="fa-brands fa-square-instagram"></i>
+                </div>
+            </footer>
+        
+        </>
     );
 
 };
